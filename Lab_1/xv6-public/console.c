cb54f1f14bad491ef349ba053f8c8054b9f60370
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-03-01 22:10:54 +0330

    Show stored command after CTRL+H entered

--- Lab_1/xv6-public/console.c
+++ Lab_1/xv6-public/console.c
@@ -14,6 +14,7 @@
 #include "mmu.h"
 #include "proc.h"
 #include "x86.h"
+#include "console.h"
 
 static void consputc(int);
 
@@ -51,6 +52,40 @@ printint(int xx, int base, int sign)
 //PAGEBREAK: 50
 
 // Print to the console. only understands %d, %x, %p, %s.
+
+#define HISTORY_SIZE 5
+#define CMD_MAX_LEN  128
+
+char history[HISTORY_SIZE][CMD_MAX_LEN];
+int history_count = 0;
+
+// Store commands in history
+void add_to_history(char *cmd) {
+  if (history_count < HISTORY_SIZE) {
+    strncpy(history[history_count], cmd, CMD_MAX_LEN);
+    history_count++;
+  }
+}
+
+void show_command_history(void) {
+  consputc('\n');
+
+  char msg[] = "[Command History]\n";
+  for (int i = 0; msg[i] != '\0'; i++) {
+    consputc(msg[i]);
+  }
+
+  for (int i = 0; i < history_count; i++) {
+    char buf[CMD_MAX_LEN + 10];
+    
+    snprintf(buf, "%d: %s\n", i + 1, history[i]);
+
+    for (int j = 0; buf[j] != '\0'; j++) {
+      consputc(buf[j]);
+    }
+  }
+}
+
 void
 cprintf(char *fmt, ...)
 {
@@ -207,12 +242,16 @@ consoleintr(int (*getc)(void))
         consputc(BACKSPACE);
       }
       break;
-    case C('H'): case '\x7f':  // Backspace
-      if(input.e != input.w){
+
+    case C('H'): case '\x7f':  // Backspace or Ctrl + H
+      if (input.e != input.w) {
         input.e--;
         consputc(BACKSPACE);
+      } else {
+        show_command_history();  // Print history if buffer is empty
       }
       break;
+
     default:
       if(c != 0 && input.e-input.r < INPUT_BUF){
         c = (c == '\r') ? '\n' : c;
