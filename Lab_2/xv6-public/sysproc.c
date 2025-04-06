5b10dc66780d536c166739058707830001c1e4d9
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-04-06 04:06:50 +0330

    Implement the system-call logic

--- Lab_2/xv6-public/sysproc.c
+++ Lab_2/xv6-public/sysproc.c
@@ -89,3 +89,75 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int sys_make_user_syscall(void) {
+  int user_id;
+  char *password;
+
+  if (argint(0, &user_id) < 0 || argstr(1, &password) < 0)
+    return -1;
+
+  for (int i = 0; i < MAX_USERS; i++) {
+    if (users[i].in_use && users[i].id == user_id)
+      return -1;
+  }
+
+  for (int i = 0; i < MAX_USERS; i++) {
+    if (!users[i].in_use) {
+      users[i].id = user_id;
+      safestrcpy(users[i].password, password, MAX_PASSWORD_LEN);
+      users[i].in_use = 1;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int sys_login_syscall(void) {
+  int user_id;
+  char *password;
+
+  if (argint(0, &user_id) < 0 || argstr(1, &password) < 0)
+    return -1;
+
+  for (int i = 0; i < MAX_USERS; i++) {
+    if (users[i].in_use && users[i].id == user_id &&
+        strncmp(users[i].password, password, MAX_PASSWORD_LEN) == 0) {
+      current_user_id = user_id;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+int sys_logout_syscall(void) {
+  if (current_user_id != -1) {
+    current_user_id = -1;
+    return 0;
+  }
+  return -1;
+}
+
+
+int sys_get_logs_syscall(void) {
+  if (current_user_id != -1) {
+    for (int i = 0; i < MAX_USERS; i++) {
+      if (logs[i].user_id == current_user_id) {
+        for (int j = 0; j < logs[i].count; j++) {
+          cprintf("Syscall %d\n", logs[i].syscalls[j]);
+        }
+        return 0;
+      }
+    }
+  } else {
+    for (int i = 0; i < MAX_USERS; i++) {
+      if (logs[i].count > 0) {
+        cprintf("User %d:\n", logs[i].user_id);
+        for (int j = 0; j < logs[i].count; j++) {
+          cprintf("  Syscall %d\n", logs[i].syscalls[j]);
+        }
+      }
+    }
+  }
+  return 0;
+}
